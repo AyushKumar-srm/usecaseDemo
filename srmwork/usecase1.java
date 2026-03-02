9a1c4435f22159e74738e6d207e577949a82c2e0
AyushKumar-srm <ak2697@srmist.edu.in>
2026-03-02 19:46:11 +0530

    My first commit

---
+++ srmwork/usecase1.java
@@ -0,0 +1,16 @@
+
+
+
+public class  usecase1{
+
+    /**
+     * Main method - Entry point of the application
+     * @param args Command line arguments
+     */
+    public static void main(String[] args) {
+
+        // Print OOPS to console
+        System.out.println("OOPS");
+
+    }
+}
