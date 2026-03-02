d5c94d62721e67f53f3d282079834039b0499d6c
AyushKumar-srm <ak2697@srmist.edu.in>
2026-03-02 20:43:24 +0530

    Added new file

---
+++ srmwork/usecase2.java
@@ -0,0 +1,17 @@
+
+public class usecase2 {
+
+    public static void main(String[] args) {
+
+        // Each letter is 7 lines high and 9 characters wide
+
+        System.out.println("  *****   " + "  *****   " + "  ******  " + "  ******  ");
+        System.out.println(" *     *  " + " *     *  " + " *     *  " + " *        ");
+        System.out.println(" *     *  " + " *     *  " + " *     *  " + " *        ");
+        System.out.println(" *     *  " + " *     *  " + "  ******  " + "  *****   ");
+        System.out.println(" *     *  " + " *     *  " + " *        " + "       *  ");
+        System.out.println(" *     *  " + " *     *  " + " *        " + " *     *  ");
+        System.out.println("  *****   " + "  *****   " + " *        " + "  *****   ");
+
+    }
+}
