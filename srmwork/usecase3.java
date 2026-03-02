847bb4c0224b5ea9f34242b699b36ec8639f101f
AyushKumar-srm <ak2697@srmist.edu.in>
2026-03-02 20:46:26 +0530

    Added new file

---
+++ srmwork/usecase3.java
@@ -0,0 +1,50 @@
+package srmwork;
+
+public class usecase3 {
+ 
+    public static void main(String[] args) {
+
+        System.out.println(String.join("",
+                "  *****   ",
+                "  *****   ",
+                "  ******  ",
+                "  ******  "));
+
+        System.out.println(String.join("",
+                " *     *  ",
+                " *     *  ",
+                " *     *  ",
+                " *        "));
+
+        System.out.println(String.join("",
+                " *     *  ",
+                " *     *  ",
+                " *     *  ",
+                " *        "));
+
+        System.out.println(String.join("",
+                " *     *  ",
+                " *     *  ",
+                "  ******  ",
+                "  *****   "));
+
+        System.out.println(String.join("",
+                " *     *  ",
+                " *     *  ",
+                " *        ",
+                "       *  "));
+
+        System.out.println(String.join("",
+                " *     *  ",
+                " *     *  ",
+                " *        ",
+                " *     *  "));
+
+        System.out.println(String.join("",
+                "  *****   ",
+                "  *****   ",
+                " *        ",
+                "  *****   "));
+    }
+}
+
